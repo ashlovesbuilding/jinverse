a2f9b3d66966e85daffaf8493ac91502a11a8c14
ashlovesbuilding <sportsfeverworld@gmail.com>
2026-09-04 15:47:55 +0530

    Add main source files

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/layout/Header.jsx'
+import Footer from './components/layout/Footer.jsx'
+import Home from './pages/Home.jsx'
+import Explore from './pages/Explore.jsx'
+import Teachings from './pages/Teachings.jsx'
+import Tirthankaras from './pages/Tirthankaras.jsx'
+import History from './pages/History.jsx'
+import Texts from './pages/Texts.jsx'
+import Articles from './pages/Articles.jsx'
+import ArticleDetail from './pages/ArticleDetail.jsx'
+import Reels from './pages/Reels.jsx'
+import About from './pages/About.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+export default function App() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/teachings" element={<Teachings />} />
+          <Route path="/tirthankaras" element={<Tirthankaras />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/texts" element={<Texts />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/articles/:slug" element={<ArticleDetail />} />
+          <Route path="/reels" element={<Reels />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
