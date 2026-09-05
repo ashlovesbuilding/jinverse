e53cf800a03d98febb15dc985a3bffce14a625c8
ashlovesbuilding <sportsfeverworld@gmail.com>
2026-09-05 06:32:00 +0530

    Add complete Tirthankara names and emblems

---
+++ src/data/tirthankaraList.js
@@ -0,0 +1,30 @@
+// Traditional names and emblems of the 24 Tirthankaras of the present cosmic age.
+// Names and emblems follow the Wikipedia list of Tirthankaras; spelling variants
+// are retained where commonly used in Jain literature.
+
+export const tirthankaraList = [
+  { number: 1, name: 'Rishabhanatha (Adinatha)', emblem: 'Bull' },
+  { number: 2, name: 'Ajitanatha', emblem: 'Elephant' },
+  { number: 3, name: 'Sambhavanatha', emblem: 'Horse' },
+  { number: 4, name: 'Abhinandananatha', emblem: 'Monkey' },
+  { number: 5, name: 'Sumatinatha', emblem: 'Flamingo' },
+  { number: 6, name: 'Padmaprabha', emblem: 'Lotus' },
+  { number: 7, name: 'Suparshvanatha', emblem: 'Swastika' },
+  { number: 8, name: 'Chandraprabha', emblem: 'Crescent moon' },
+  { number: 9, name: 'Pushpadanta (Suvidhinatha)', emblem: 'Crocodile or makara' },
+  { number: 10, name: 'Shitalanatha', emblem: 'Shrivatsa or kalpavriksha' },
+  { number: 11, name: 'Shreyansanatha', emblem: 'Rhinoceros' },
+  { number: 12, name: 'Vasupujya', emblem: 'Buffalo' },
+  { number: 13, name: 'Vimalanatha', emblem: 'Boar' },
+  { number: 14, name: 'Anantanatha', emblem: 'Falcon' },
+  { number: 15, name: 'Dharmanatha', emblem: 'Vajra' },
+  { number: 16, name: 'Shantinatha', emblem: 'Deer' },
+  { number: 17, name: 'Kunthunatha', emblem: 'Goat' },
+  { number: 18, name: 'Aranatha', emblem: 'Nandyavarta' },
+  { number: 19, name: 'Mallinatha', emblem: 'Water pot' },
+  { number: 20, name: 'Munisuvratanatha', emblem: 'Tortoise' },
+  { number: 21, name: 'Naminatha', emblem: 'Blue lotus' },
+  { number: 22, name: 'Neminatha', emblem: 'Conch' },
+  { number: 23, name: 'Parshvanatha', emblem: 'Serpent' },
+  { number: 24, name: 'Mahavira', emblem: 'Lion' },
+]
